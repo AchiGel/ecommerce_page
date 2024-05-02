5be2f2a71c385c922a4d4a74d57e6319ebbc3e26
AchiGel <ar.gelashvili@gmail.com>
2024-05-02 15:35:30 +0400

    add products

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import "./index.css";
-
 import styled from "styled-components";
+import furniture_data from "./DATA/furniture_data.json";
 
 const Wrapper = styled.div`
   width: 100%;
@@ -12,11 +12,13 @@ const Wrapper = styled.div`
 `;
 
 function App() {
+  const DATA = furniture_data.furniture_items;
+
   return (
     <div className="App">
       <Header />
       <Wrapper>
-        <Main />
+        <Main data={DATA} />
         <Footer />
       </Wrapper>
     </div>
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -28,10 +28,57 @@ const SectionOne = styled.section`
   }
 `;
 
-export default function Main() {
+const ProductsSection = styled.section`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  margin-top: 50px;
+  gap: 15px;
+  justify-items: center;
+`;
+
+const ProductCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  background-color: #9e9e9e3b;
+  max-width: 250px;
+  padding: 15px;
+  border-radius: 10px;
+  overflow: hidden;
+`;
+
+const ProductImg = styled.img`
+  width: 100%;
+  height: 200px;
+  object-fit: cover;
+`;
+
+interface FurnitureItem {
+  ID: number;
+  title: string;
+  description: string;
+  price: number;
+  img: string;
+}
+
+interface Props {
+  data: FurnitureItem[];
+}
+
+export default function Main(props: Props) {
   return (
     <MainContainer>
       <SectionOne></SectionOne>
+      <ProductsSection>
+        {props.data.map((el) => (
+          <ProductCard key={el.ID}>
+            <ProductImg src={"assets/" + el.img} alt={el.title} />
+            <h1>{el.title}</h1>
+            <p>{el.description}</p>
+            <span>{"$" + el.price}</span>
+          </ProductCard>
+        ))}
+      </ProductsSection>
     </MainContainer>
   );
 }
